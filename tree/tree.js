887ee9b0d134579db04e11571a70806d0f08b0bc
Abu Rayhan Kobir <aburayhankobir013@gmail.com>
2026-09-04 20:59:42 +0600

    Tree data structure preOrder, postOrder and inorder traversal included

--- tree/tree.js
+++ tree/tree.js
@@ -1,25 +1,48 @@
 class Node {
   constructor (data) {
+    this.data = data;
     this.left = null;
     this.right = null;
-    this.data = data;
   }
 }
+
+const root = new Node (10);
+root.left = new Node (20);
+root.left.left = new Node (30);
+root.left.right = new Node (40);
+root.right = new Node (50);
+root.right.left = new Node (60);
+root.right.right = new Node (70);
 
 function preOrder (root) {
   if (root !== null) {
     process.stdout.write (`${root.data} `);
-    preOrder(root.left);
-    preOrder(root.right);
+    preOrder (root.left);
+    preOrder (root.right);
   }
+  return;
 }
- 
-const root = new Node (1);
-root.left = new Node (3);
-root.right = new Node (5);
-root.left.left = new Node (2);
-root.left.right = new Node (4);
-root.right.right = new Node (8);
 
+function inorder (root) {
+  if (root !== null) {
+    inorder (root.left);
+    process.stdout.write (`${root.data} `);
+    inorder (root.right);
+  }
+  return;
+}
 
-preOrder(root);
+function postOrder (root) {
+  if (root !== null) {
+    preOrder (root.left);
+    preOrder (root.right);
+    process.stdout.write (`${root.data} `);
+  }
+  return;
+}
+
+preOrder (root);
+console.log ("\n");
+inorder (root);
+console.log ("\n");
+postOrder (root);
