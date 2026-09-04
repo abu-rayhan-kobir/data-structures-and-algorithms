52ad2857f05c369c576ad83344e01db925bd40f2
Abu Rayhan Kobir <aburayhankobir013@gmail.com>
2026-09-04 22:11:42 +0600

    Transfer code learning.js to binarySearchTree.js

--- tree/binarySearchTree.js
+++ tree/binarySearchTree.js
@@ -1,28 +1,62 @@
-class Node  {
-  constructor (data) {
+class Node {
+  constructor(data) {
     this.data = data;
     this.left = null;
     this.right = null;
   }
+}
 
-  insert (root, data) {
-    if (root === null) {
-      return new Node (data);
-    }
-    if (root.data === data) {
-      return root;
-    }
-    if (root.data > data) {
-      root.left = this.insert (root.left, data);
-    } else {
-      root.right = this.insert (root.right, data);
-    }
+function insert(root, data) {
+  if (root === null) {
+    return new Node(data);
+  }
+  if (root.data === data) {
     return root;
+  }
+  if (root.data > data) {
+    root.left = insert(root.left, data);
+  } else {
+    root.right = insert(root.right, data);
+  }
+  return root;
+}
+
+function search (root, data) {
+  if (root === null) {
+    console.log ("Element Not Found!");
+    return;
+  }
+  if (root.data === data) {
+    console.log ("Element Found!");
+    return;
+  }
+  if (root.data > data) {
+    search (root.left, data);
+  } else {
+    search (root.right, data);
   }
 }
 
-const root = new Node (20);
-root.left = new Node (15);
-root.right = new Node (30);
-root.left.left = new Node (12);
-root.left.right = new Node (30);
+let root = insert (null, 12);
+root = insert (root, 40);
+root = insert (root, 13);
+root = insert (root, 19);
+root = insert (root, 21);
+root = insert (root, 25);
+root = insert (root, 29);
+root = insert (root, 9);
+root = insert (root, 2);
+
+function inorder(root) {
+  if (root !== null) {
+    inorder(root.left);
+    process.stdout.write(`${root.data} `);
+    inorder(root.right);
+  }
+  return;
+}
+
+inorder (root);
+console.log ("\n");
+search (root, 19);
+search (root, 4);
